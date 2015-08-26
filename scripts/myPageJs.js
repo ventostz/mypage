04be4fd41b004c53cd2cf36cb8d3f241486c1456
Plamen Gerdzhikov <typa_po6ta@yahoo.com>
2015-08-26 17:46:34 +0300

    footer ready

--- scripts/myPageJs.js
+++ scripts/myPageJs.js
@@ -1,88 +1,3 @@
-/*var a = new Date();
-var date = a.getHours();
-
-if (date < 2) {
-	$("body").css("background-image" , "url('0.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 4) {
-	$("body").css("background-image" , "url('1.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 6) {
-	$("body").css("background-image" , "url('2.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 8) {
-	$("body").css("background-image" , "url('3.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 10) {
-	$("body").css("background-image" , "url('4.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 12) {
-	$("body").css("background-image" , "url('6.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 14) {
-	$("body").css("background-image" , "url('6.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 16) {
-	$("body").css("background-image" , "url('7.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 18) {
-	$("body").css("background-image" , "url('8.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 20) {
-	$("body").css("background-image" , "url('9.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else if (date < 22) {
-	$("body").css("background-image" , "url('day.jpg')");
-	$("body").css("background-size" , "cover");
-}
-else {
-	$("body").css("background-image" , "url('night2.jpg')");
-	$("body").css("background-size" , "cover");
-};*/
-
-
-/*$(function(){
-     
-    var picsArray = new Array();
-     
-    picsArray[0]="0.jpg";
-    picsArray[1]="1.jpg";
-    picsArray[2]="2.jpg";
-    picsArray[3]="3.jpg";
-    picsArray[4]="4.jpg";
-    picsArray[5]="5.jpg";
-    picsArray[6]="6.jpg";
-    picsArray[7]="7.jpg";
-    picsArray[8]="8.jpg";
-    picsArray[9]="9.jpg";
-
-    var thisId = 0;
-
-    window.setInterval(function(){
-        $("body").css("background-image", "url('"+picsArray[thisId]+"')");
-        $("body").css("background-size" , "cover");
-        thisId++;
-        if (thisId == 10) thisId = 0;
-    },20000);        
- });*/
- 
- 
- /*carousel buttons*/
- /*$(".leftbutton").click(function() {
-	if ()
-	$(".slidelist").css()
- })*/
- 
  var slideWidth = 1000;
  
  var slideToggle = function(dir) {
@@ -136,3 +51,11 @@ $(function() {
 	
 	sliderInit();
 });
+
+
+ /*carousel buttons*/
+ /*$(".leftbutton").click(function() {
+	if ()
+	$(".slidelist").css()
+ })*/
+ 
